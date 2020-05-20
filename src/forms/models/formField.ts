d6872f2672e91e4ea40fd10742d624d25bdf486c
nsl-umbraco <nsl@umbraco.dk>
2020-05-20 12:50:39 +0200

    make properties optional

--- src/forms/models/formField.ts
+++ src/forms/models/formField.ts
@@ -1,10 +1,10 @@
 export class FormField {
   constructor(
     public id: string,
-    public containsSensitiveData: boolean = false,
-    public mandatory: boolean = false,
-    public requiredErrorMessage: string = '',
-    public regex: string = '',
-    public invalidErrorMessage: string = '',
+    public containsSensitiveData?: boolean = false,
+    public mandatory?: boolean = false,
+    public requiredErrorMessage?: string = '',
+    public regex?: string = '',
+    public invalidErrorMessage?: string = '',
   ) {}
 }
